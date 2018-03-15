121b5c204fd1764190ab0f8b2fd0e04e8fd695a2
NikkiNick <nick.gysels.w2819@student.hogent.be>
2018-03-15 20:06:36 +0100

    Toevoegen about / home / downloads / links / nieuws / PNF

--- applicatie/src/app/app.module.ts
+++ applicatie/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MainComponent } from './components/main/main.component';
 import { UserComponent } from './components/user/user.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { NieuwsComponent } from './components/nieuws/nieuws.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LinksComponent } from './components/links/links.component';
+import { DownloadsComponent } from './components/downloads/downloads.component';
 
 
 @NgModule({
@@ -13,7 +19,13 @@ import { UserComponent } from './components/user/user.component';
     AppComponent,
     NavigationComponent,
     MainComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent,
+    HomeComponent,
+    NieuwsComponent,
+    PageNotFoundComponent,
+    LinksComponent,
+    DownloadsComponent
   ],
   imports: [
     BrowserModule
